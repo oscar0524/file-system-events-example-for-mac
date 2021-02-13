8cb24f1fb91d7e4dcc7b761cd03b37655b1bb8bc
oscar0524 <oscar870524@gmail.com>
2021-02-13 19:53:10 +0800

    Initial Commit

---
+++ file-system-events/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  file-system-events
+//
+//  Created by 葉冠暉 on 2021/2/13.
+//  Copyright © 2021 葉冠暉. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
